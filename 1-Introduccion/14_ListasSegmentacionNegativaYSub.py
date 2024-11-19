ca3680ba7a6a829b987bc4dc6787827f1fc68407
Alfred <arista21dominguez@gmail.com>
2024-11-18 20:16:33 -0600

    Indices negativos

---
+++ 1-Introduccion/14_ListasSegmentacionNegativaYSub.py
@@ -0,0 +1,8 @@
+animales = ["gato", "perro", "pajaro", "vaca"]
+print(animales[-1])
+print(animales[-3:-1])
+
+animales[-2:] = ["tortuga", "leon", "jirafa"]
+print(animales)
+sub_animales = animales[-3:-1]
+print(sub_animales)
