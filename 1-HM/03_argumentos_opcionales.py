b27431ca8473e187c22d5d37367ebc44a3e67011
Alfred <arista21dominguez@gmail.com>
2025-02-16 18:59:41 -0600

    Parametros de funciones

---
+++ 1-HM/03_argumentos_opcionales.py
@@ -0,0 +1,9 @@
+def hola(sex, nombre="Alfred"):
+    print(f"Hola {nombre}")
+    print("eres", sex)
+
+
+hola("mujer", "Maria")
+hola(nombre="Rios", sex="hombre")
+
+hola("hombre", nombre="Rios")
---
+++ 1-HM/04_xargs.py
@@ -0,0 +1,8 @@
+def suma(*numeros):
+    res = 0
+    for i in numeros:
+        res += i
+    print(res)
+
+
+suma(43, 45, 43, 3)
